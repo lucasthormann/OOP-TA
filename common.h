a7f31ae102c010ef7fd560f202d4d9703b908c5b
lucasthormann <LucasThormann@protonmail.com>
2024-08-02 10:47:05 -0500

    common header

---
+++ common.h
@@ -0,0 +1,17 @@
+#ifndef _COMMON_
+#define _COMMON_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+
+using namespace std;
+
+#endif
