f451e43d2c3885547398ec8e7eb23fbf252f0ccf
lucasthormann <LucasThormann@protonmail.com>
2024-08-03 12:06:48 -0500

    User header documentation

--- User.h
+++ User.h
@@ -7,11 +7,11 @@
 class User{
 
 private:
-  string name;
-  string email;
+  string name; // holds name of user
+  string email; // holds user email
   double boughtPrice;
-  double totalValue;
-  double netValue;
+  double totalValue; // portfolio total value
+  double netValue; // profit or loss of entire portfolio
   vector<Industry> purchasedStocks;
   vector<Industry> soldStocks;
 
@@ -23,9 +23,9 @@ public:
   string getEmail();
 
   friend void showUserStats(User user); // friend function used here which returns portfolio info
-                                        //
-  void adjustTotalValue();
-  void adjustNetValue();
+                                        
+  void adjustTotalValue(); // calculates total value of portfolio as transactions are made
+  void adjustNetValue(); // calculates the net profit in the portfolio as transactions are made
 
   double getTotalValue();
   void setTotalValue(double totalValue);
@@ -33,11 +33,11 @@ public:
 
   double getNetValue();
 
-  void priceChangeAllPurchasedStocks();
+  void priceChangeAllPurchasedStocks(); // applies the price changing algo to all purchased stocks
 
-  friend User& operator += (User& user, Industry st);
+  friend User& operator += (User& user, Industry st); // overloaded operators and friend function used here to add a stock to the portfolio when it's bought
 
-  friend User& operator -= (User& user, Industry st);
+  friend User& operator -= (User& user, Industry st); // sells a stock and removes it from portfolio
 
 
   void printPurchasedStockList();
